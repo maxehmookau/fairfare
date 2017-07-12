4c8e1402309dfd3ded44c818001ef8aaa4b6c624
Max Woolf <max@woolf.io>
2017-07-12 20:29:50 +0100

    inital commit

---
+++ app.rb
@@ -0,0 +1,22 @@
+require 'sinatra'
+require 'yaml'
+require 'kramdown'
+
+get '/' do
+  erb :index
+end
+
+get '/question/:name?' do
+  question_name = params[:name] || 'cheating'
+  @question = YAML.load_file("questions/#{question_name}.yml")
+  erb :question, layout: :layout
+end
+
+get '/question/:name/yes' do
+  @question = YAML.load_file("questions/#{params[:name]}.yml")
+  erb :yes, layout: :layout
+end
+
+get '/lawyer' do
+  erb :lawyer, layout: :layout
+end
